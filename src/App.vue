d7498d9637bc3815ede516ed5efecd1c342e5ab1
tomokazu.tantaka <tantaka.tomokazu@gmail.com>
2019-06-05 21:24:40 +0900

    second commit

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,10 @@
         <span class="font-weight-light">MATERIAL DESIGN</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
+      <v-btn flat href="http://nvd3.org" target="_blank" >
         <span class="mr-2">Latest Release</span>
       </v-btn>
     </v-toolbar>
-
     <v-content>
       <HelloWorld/>
     </v-content>
